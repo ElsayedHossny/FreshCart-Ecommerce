12de71b8be32b28263689503b4917b7a48a96528
Elsayed <elsayedhossny90@gmail.com>
2026-05-24 23:25:55 +0300

    Feature: Integrate React Query for product fetching and add FeatureProducts component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,14 @@
-import React, { useContext } from 'react'
-import Style from "./Home.module.css"
-import { CounterContext } from '../../Context/Contect'
+import React from 'react'
 
+import FeatureProducts from '../FeatureProducts/FeatureProducts'
 
 
 
 export default function Home() {
 
-  let { Counter, setCounter } = useContext(CounterContext);
 
   return (<>
-    <div>Home:{Counter}</div>
-    <button onClick={() => setCounter(Math.floor(Math.random() * 10))} className='btn btn-info'>Change</button>
-
+    <FeatureProducts />
   </>
   )
 }

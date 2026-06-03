a182f67cc2fd486f32ec7c185dc39e1f4c32425c
Elsayed <elsayedhossny90@gmail.com>
2026-06-04 01:15:03 +0300

    modify UI.

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function Brands() {
-  return (
-    <div>
-      <p>sauuu</p>
-    </div>
+  return (<>
+    <h2>Elsayed </h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur in odit maxime, nostrum excepturi magnam amet autem deserunt voluptatum inventore?</p>
+  </>
   )
 }

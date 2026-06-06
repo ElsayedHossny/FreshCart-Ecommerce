ca2ab511cbce0875c5170067aacbc00a9a5b6c3f
Elsayed <elsayedhossny90@gmail.com>
2026-06-06 06:33:02 +0300

    Add Swiper integration for sliders and update styles

--- src/components/MainSlider/MainSlider.jsx
+++ src/components/MainSlider/MainSlider.jsx
@@ -4,41 +4,33 @@ import img2 from "../../images/slider-image-2.jpeg"
 import img3 from "../../images/slider-image-3.jpeg"
 import bolg1 from "../../images/grocery-banner.png"
 import blog2 from "../../images/grocery-banner-2.jpeg"
-
-
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css';
 
 export default function MainSlider() {
 
-
-
-  var settings = {
-    dots: true,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
-
-  return <div className="mt-3">
-    <div className="row" >
-      <div className="col-md-9 ">
-        <Slider {...settings}>
-          <img height={400} className="w-100 " src={img1} alt="" />
-          <img height={400} className="w-100" src={img2} alt="" />
-          <img height={400} className="w-100" src={img3} alt="" />
-        </Slider>
-      </div>
-      <div className="d-none d-md-flex col-md-3 p-0 flex-column gap-1">
-        <img height={198} className="w-100 cover-full" src={bolg1} alt="" />
-        <img height={198} className="w-100 cover-full" src={blog2} alt="" />
+  return (
+    <div className="mt-3">
+      <div className="row">
+        <div className="col-md-9">
+          <Swiper
+            style={{ paddingBottom: '40px' }}
+            modules={[Autoplay, Pagination]}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 2000, disableOnInteraction: false }}
+            spaceBetween={10}
+          >
+            <SwiperSlide><img height={400} className="w-100" style={{ objectFit: 'cover' }} src={img1} alt="" /></SwiperSlide>
+            <SwiperSlide><img height={400} className="w-100" style={{ objectFit: 'cover' }} src={img2} alt="" /></SwiperSlide>
+            <SwiperSlide><img height={400} className="w-100" style={{ objectFit: 'cover' }} src={img3} alt="" /></SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="d-none d-md-flex col-md-3 p-0 flex-column gap-1">
+          <img height={198} className="w-100" style={{ objectFit: 'cover' }} src={bolg1} alt="" />
+          <img height={198} className="w-100" style={{ objectFit: 'cover' }} src={blog2} alt="" />
+        </div>
       </div>
     </div>
-  </div>
-
+  );
 }
-

687c130acd29858674234e24a4ef04b6b9924034
Elsayed Hossny <105835371+ElsayedHossny@users.noreply.github.com>
2026-09-18 00:19:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // رفع حد التحذير إلى 1000kB (1MB) بدلاً من 500kB
+    chunkSizeWarningLimit: 1000,
+  },
 });
